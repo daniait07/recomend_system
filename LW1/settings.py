17720d74f91ef533dd0583228c706e465364b20c
Даниил Петровский <120306367+daniait07@users.noreply.github.com>
2025-10-27 00:10:32 +0300

    Update settings.py

--- LW1/settings.py
+++ LW1/settings.py
@@ -1,41 +1,13 @@
-import os
-from dataclasses import dataclass
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+from pydantic import Field
 
-# Загрузка .env из корня проекта 
-load_dotenv()
+class Settings(BaseSettings):
+    GEMINI_API_KEY: str = Field(..., description="Google Gemini API key")
+    NLP_CLOUD_API_KEY: str = Field(..., description="NLP Cloud API key")
+    
+    NLP_CLOUD_URL: str = 'https://api.nlpcloud.io/v1'
 
+    class Config:
+        env_file = ".env"
 
-@dataclass(frozen=True)
-class NLPCloudConfig:
-    base_url: str = os.getenv("NLPCLOUD_BASE_URL", "https://api.nlpcloud.io/v1")
-    api_key: str = os.getenv("NLPCLOUD_API_KEY", "")
-    model: str = os.getenv("NLPCLOUD_MODEL", "en_core_web_lg")
-    timeout: float = float(os.getenv("TIMEOUT_SECONDS", "15.0"))
-
-
-@dataclass(frozen=True)
-class HuggingFaceConfig:
-    model: str = os.getenv("HF_MODEL", "dbmdz/bert-large-cased-finetuned-conll03-english")
-    token: str = os.getenv("HF_API_TOKEN", "")
-    use_local_fallback: bool = bool(int(os.getenv("HF_USE_LOCAL_FALLBACK", "0")))
-
-
-@dataclass(frozen=True)
-class MetricsConfig:
-    file_path: str = os.getenv("METRICS_FILE", "./logs/metrics.jsonl")
-
-
-@dataclass(frozen=True)
-class AppSettings:
-    """Глобальные настройки приложения."""
-    env: str = os.getenv("APP_ENV", "dev")
-    log_level: str = os.getenv("LOG_LEVEL", "INFO")
-
-    nlpcloud: NLPCloudConfig = NLPCloudConfig()
-    huggingface: HuggingFaceConfig = HuggingFaceConfig()
-    metrics: MetricsConfig = MetricsConfig()
-
-
-# Глобальный объект конфигурации 
-settings = AppSettings()
+settings = Settings()
